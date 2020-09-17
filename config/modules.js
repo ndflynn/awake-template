675485afb6c24d6d774692b9b3992e5cc5442b32
Niall Flynn <ndflynn@gmail.com>
2020-09-17 19:40:21 -0400

    gmap update

--- config/modules.js
+++ config/modules.js
@@ -15,7 +15,8 @@ export const modules = [
   '@nuxtjs/eslint-module',
   '@nuxtjs/style-resources',
   'nuxt-fontawesome',
-  'nuxt-responsive-loader'
+  'nuxt-responsive-loader',
+  'nuxt-gmaps'
 ]
 if (siteConfig.googleAnalytics.on && siteConfig.googleAnalytics.id) {
   modules.push('@nuxtjs/google-gtag')
@@ -50,5 +51,8 @@ export const modulesSettings = {
   },
   'google-gtag': {
     id: siteConfig.googleAnalytics.id
+  },
+  'nuxt-gmaps': {
+    key: 'AIzaSyAFg-9-NP8cjHAbzOOOHTYyiyMTzTOCnr8'
   }
 }
