7e2057170bbc7f99482450eb913447ab476e64da
Niall Flynn <ndflynn@gmail.com>
2020-10-01 10:45:40 -0400

    Update HomeInstagram.vue

--- components/HomeInstagram.vue
+++ components/HomeInstagram.vue
@@ -26,10 +26,10 @@ export default {
 </script>
 <style lang="scss" scoped>
 .container,
-#home-instagram {
-  height: 400px;
-}
-.vue-iframe iframe {
+.vue-iframe,
+.vue-iframe iframe,
+iframe#home-instagram {
+  width: 100%;
   height: 400px;
 }
 </style>
